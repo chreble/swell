a7f37e4a38f0ec30b027c92a979105908735f7f0
jgautheron <jgautheron@438c34f2-0ad0-4f89-b8de-58cde357e140>
2009-02-11 08:48:29 +0000

    added DOM

---
+++ Core/Dom.js
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2009, Nexway All rights reserved.
+ * Code licensed under the BSD License:
+ * http://ui.nexway.com/license.txt
+ * version: 1.0 beta
+ *
+ * @author <a href="mailto:christopheeble@gmail.com">Christophe Eble</a>
+ * @author <a href="mailto:alpherz@gmail.com">Jonathan Gautheron</a>
+*/
+
+NXUI.setNameSpace('Api.Dom');
+
+NXUI.Api.Class({
+    
+    name      : 'DomObject',
+    namespace : 'Api',
+    functions : {
+    
+        /**
+         * @constructor
+        */
+        construct : function(e) {
+        }
+    }
+});
+
+NXUI.Api.Dom = new function(){
+    
+    var _registeredElements = {};
+    
+    return {
+    
+        get : function(el, domObject) {
+            if (NXUI.Api.isString(el)) {
+                return document.getElementById(el);
+            }
+            
+            if (NXUI.Api.isArray(el)) {
+                var _i = el.length, _els = [];
+                while (i--) {
+                    _els.push(this.get(el[_i]));
+                }
+                return _els;
+            }
+        },
+        
+        hasClass : function(el, className) {
+            if (NXUI.Api.isString(el)) {
+                el = this.get(el);
+            }
+            
+            if (!NXUI.Api.isUndefined(el.nodeType)) {
+                var _expr = new RegExp('([^\s]|[^\w])?(' + className + ')([^\s]|[^\w])?');
+                return _expr.test(el.className);
+            }
+        },
+        
+        addClass : function(el, className) {
+            if (!NXUI.Api.isUndefined(el.nodeType)) {
+                if (NXUI.Api.isArray(className)) {
+                    var _l = className.length;
+                    while (_l--) {
+                        this.addClass(el, className[_l]);
+                    }
+                    return;
+                }
+                if (this.hasClass(el, className)) {
+                    return false;
+                }
+                
+                el.className = [el.className, className].join(' ');
+                return;
+            }
+            
+            if (NXUI.Api.isString(el)) {
+                this.addClass(this.get(el), className);
+            }
+            
+            if (NXUI.Api.isArray(el)) {
+                var _i = el.length;
+                while (_i--) {
+                    this.addClass(el[_i], className);
+                }
+            }
+        },
+        
+        removeClass : function(el, className) {
+            
+        }
+        
+    }
+    
+}();
+
+// Set some shorthands for the class
+NXUI.setAlias(NXUI.Api.Dom, 'Dom');
