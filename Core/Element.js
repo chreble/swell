0c99dcc907fda06be06b74857bca772e2ac67045
jgautheron <jgautheron@438c34f2-0ad0-4f89-b8de-58cde357e140>
2009-03-07 17:25:28 +0000

    added Element

---
+++ Core/Element.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2009, Swell All rights reserved.
+ * Code licensed under the BSD License:
+ * http://www.justswell.org/license.txt
+ * version: 1.0 beta
+ *
+ * @author <a href="mailto:christopheeble@gmail.com">Christophe Eble</a>
+ * @author <a href="mailto:alpherz@gmail.com">Jonathan Gautheron</a>
+*/
+Swell.namespace('Core.Element');
+
+Swell.Core.Class({
+    
+    name      : 'Element',
+    namespace : 'Core',
+    functions : function() {
+        
+        /** @lends Swell.Core.Element.prototype */
+        return {
+            
+            /** @constructs */
+            construct : function(name, scope) {
+                
+            }
+        }
+        
+    }()
+    
+});
+
+// Set some shorthands for the class
+Swell.alias(Swell.Core.Element, 'Element');
